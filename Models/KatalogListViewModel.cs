07bc37533561ecf5b6c962dca222b7a7857bce94
Баглай Павел <32196367+pavelbaglai@users.noreply.github.com>
2021-10-03 18:53:38 +0300

    commi
    --

---
+++ Models/KatalogListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+ 
+
+namespace WebApplication39.Models
+{
+    public class KatalogListViewModel
+    {
+        public IEnumerable<Katalog> Katalogs { get; set; }
+        public SelectList Categories { get; set; }
+        public string Name { get; set; }
+    }
+}
